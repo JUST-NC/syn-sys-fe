586bfa180fedbf2e329476c4b8c23185351e18aa
Skye Young <iskyex@outlook.com>
2021-03-31 11:12:26 +0800

    refactor: change the way of routing management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import { route, RouteModel } from './routes';
+import { HOME_PAGE, LOGIN_PAGE, route, RouteModel } from './routes';
 import { observer } from 'mobx-react-lite';
 import { userStore } from './stores/user-store';
 
@@ -14,12 +14,13 @@ const RouteWithSubRoutes = observer(
   ({ Component, path, auth = false, exact = false, ...rest }: RouteModel) => (
     <Route exact={exact} path={path}>
       {({ location }) => {
+        // 需要认证 且 未登录
         if (auth && !userStore.logged) {
-          // 需要认证 且 未登录
-          return <Redirect to="/login" />;
-        } else if (userStore.logged && location.pathname === '/login') {
-          //  已登录 且 路径为 /login
-          return <Redirect to="/" />;
+          return <Redirect to={LOGIN_PAGE.path} />;
+        }
+        //  已登录 且 路径为 /login
+        else if (userStore.logged && location.pathname === LOGIN_PAGE.path) {
+          return <Redirect to={HOME_PAGE.path} />;
         } else {
           return <Component {...rest} />;
         }
