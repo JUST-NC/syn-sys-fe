b5c0ce60e9133f067cfa46d477416eb4d72970a5
Skye Young <iskyex@outlook.com>
2021-04-02 01:50:06 +0800

    refactor: add tailwind and try it out

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/main.scss';
+import './styles/main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
