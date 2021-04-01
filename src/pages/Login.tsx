0d451fb1cb676a4f03a09f13d2ccae0b5231a56e
Skye Young <iskyex@outlook.com>
2021-04-02 01:50:06 +0800

    refactor: add tailwind and try it out

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,12 @@
 import { Helmet } from 'react-helmet-async';
 import { LoginForm } from '../components/LoginForm';
-import { Paper, Typography } from '@material-ui/core';
+import { Link, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 import styles from '../styles/Login.module.scss';
 import Div100vh from 'react-div-100vh';
+import classNames from 'classnames';
+import { Link as RouterLink } from 'react-router-dom';
+import { HOME_PAGE } from '../routes';
 
 const Login: React.FC = () => {
   return (
@@ -14,16 +17,30 @@ const Login: React.FC = () => {
 
       <Paper className={styles.loginPaper} component={Div100vh}>
         <div>
-          <Typography variant={'h6'} gutterBottom={true}>
+          <Typography
+            className={classNames('text-gray-500')}
+            variant={'h6'}
+            gutterBottom={true}
+          >
             江苏科技大学
           </Typography>
-          <Typography variant={'h3'} gutterBottom={true}>
+          <Typography
+            className={classNames('text-gray-800')}
+            variant={'h3'}
+            gutterBottom={true}
+          >
             欢迎回来
           </Typography>
         </div>
 
         <div>
           <LoginForm />
+        </div>
+
+        <div className={classNames('text-center')}>
+          <Link color={'initial'} component={RouterLink} to={HOME_PAGE.path}>
+            忘记密码？
+          </Link>
         </div>
       </Paper>
     </>
