8c8c76e0d0893656dd99b9e86e4523743473a1eb
Skye Young <iskyex@outlook.com>
2021-04-17 23:42:56 +0800

    fix: try to fix findDOMNode warning
    It's still show warning when rendering DatePicker, but I choose to keep it for possible issues.

--- src/index.tsx
+++ src/index.tsx
@@ -5,27 +5,37 @@ import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 import localforage from 'localforage';
 import { GlobalStyles } from 'twin.macro';
-import { StylesProvider } from '@material-ui/core/styles';
-import { css, Global } from '@emotion/react';
+import {
+  StylesProvider,
+  unstable_createMuiStrictModeTheme,
+} from '@material-ui/core/styles';
+import { css, Global, ThemeProvider } from '@emotion/react';
+import { zhCN } from '@material-ui/core/locale';
 
 // 数据库名初始化
 localforage.config({
   name: 'syn-sys',
 });
 
+// 处理某些组件在严格模式下产生 warning 的问题
+// 同时提供一些官方的本地化……我也不知道有啥用
+const theme = unstable_createMuiStrictModeTheme({}, zhCN);
+
 ReactDOM.render(
   <React.StrictMode>
     <HelmetProvider>
       <StylesProvider injectFirst>
-        <GlobalStyles />
-        <Global
-          styles={css`
-            button:focus {
-              outline: none !important;
-            }
-          `}
-        />
-        <App />
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <Global
+            styles={css`
+              button:focus {
+                outline: none !important;
+              }
+            `}
+          />
+          <App />
+        </ThemeProvider>
       </StylesProvider>
     </HelmetProvider>
   </React.StrictMode>,
