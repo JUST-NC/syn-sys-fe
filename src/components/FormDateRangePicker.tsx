815b31dbc9070b07778981f354e3beea26f3c0e9
Skye Young <iskyex@outlook.com>
2021-04-18 17:11:32 +0800

    refactor(formdaterangepicker): rewrite notes and some props

--- src/components/FormDateRangePicker.tsx
+++ src/components/FormDateRangePicker.tsx
@@ -6,7 +6,12 @@ import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import Box from '@material-ui/core/Box';
 import { FormLabel, Typography } from '@material-ui/core';
 import 'twin.macro';
+import { constant } from '../utils/constant';
+import { observer } from 'mobx-react-lite';
 
+/**
+ * 从属性中排除一些公共值
+ */
 type DateProps = Omit<
   DatePickerProps,
   | 'label'
@@ -16,6 +21,9 @@ type DateProps = Omit<
   | 'disableFuture'
   | 'showTodayButton'
   | 'todayLabel'
+  | 'cancelLabel'
+  | 'okLabel'
+  | 'animateYearScrolling'
 >;
 
 export interface FormDateRangePickerProps {
@@ -29,9 +37,15 @@ export interface FormDateRangePickerProps {
   showTodayButton?: boolean;
   todayLabel?: string;
   betweenLabel?: string;
+  cancelLabel?: string;
+  okLabel?: string;
+  animateYearScrolling?: boolean;
 }
 
-const StyledDatePicker: React.FC<DatePickerProps> = ({ ...props }) => (
+/**
+ * 添加了一些简单样式的日期选择器
+ */
+const StyledDatePicker = observer<DatePickerProps>(({ ...props }) => (
   <DatePicker
     tw={'w-auto'}
     InputLabelProps={{
@@ -39,31 +53,45 @@ const StyledDatePicker: React.FC<DatePickerProps> = ({ ...props }) => (
     }}
     {...props}
   />
-);
+));
 
-const FormDateRangePicker: React.FC<FormDateRangePickerProps> = (props) => {
-  let { label, betweenLabel, beginDateProps, endDateProps, ...commonProps } = props;
+/**
+ * 日期区间选择器
+ *
+ * 进行了中文本地化，为了方便所以有一部分是写死在里面的……
+ *
+ * 如果想修改组件的话，建议看一下 {@link https://material-ui-pickers.dev/}
+ */
+const FormDateRangePicker = observer<FormDateRangePickerProps>((props) => {
+  let {
+    label,
+    betweenLabel = '至',
+    beginDateProps,
+    endDateProps,
+    ...commonProps
+  } = props;
 
+  // 提供一些默认值
   commonProps = {
-    format: commonProps.format ?? 'yyyy-MM-dd',
+    format: commonProps.format ?? constant.DATE_FORMAT,
     todayLabel: commonProps.todayLabel ?? '回到今天',
+    cancelLabel: commonProps.cancelLabel ?? '取消',
+    okLabel: commonProps.okLabel ?? '确认',
     ...commonProps,
   };
 
   return (
-    <>
-      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={zhCN}>
-        <Box>
-          <FormLabel>{label}</FormLabel>
-          <Box tw={'flex items-center justify-center'}>
-            <StyledDatePicker {...commonProps} {...beginDateProps} />
-            <Typography tw={'inline-block mx-4'}>{betweenLabel ?? '至'}</Typography>
-            <StyledDatePicker {...commonProps} {...endDateProps} />
-          </Box>
+    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={zhCN}>
+      <Box>
+        <FormLabel>{label}</FormLabel>
+        <Box tw={'flex items-center'}>
+          <StyledDatePicker {...commonProps} {...beginDateProps} />
+          <Typography tw={'inline-block mx-4'}>{betweenLabel}</Typography>
+          <StyledDatePicker {...commonProps} {...endDateProps} />
         </Box>
-      </MuiPickersUtilsProvider>
-    </>
+      </Box>
+    </MuiPickersUtilsProvider>
   );
-};
+});
 
 export { FormDateRangePicker };
