5285fb5258de6b8256fc28a4202868a4ddf212dd
Skye Young <iskyex@outlook.com>
2021-04-19 23:20:43 +0800

    feat: formInput component
    Ready to use FormInput component to replace FormTextField

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,29 @@
+import { TextField, TextFieldProps } from 'mui-rff';
+import React from 'react';
+import { css } from '@emotion/react';
+import tw from 'twin.macro';
+
+const FormInput: React.FC<TextFieldProps> = (props) => {
+  return (
+    <TextField
+      css={css`
+        ${tw`mb-8 border-b-2 border-gray-100 border-solid`}
+        .MuiInputLabel-shrink {
+          transform: translate(0, 1.5px);
+        }
+
+        label {
+          ${tw`text-gray-500`}
+        }
+
+        label + .MuiInput-formControl {
+          ${tw`mt-6 before:(border-none!) after:(border-none!)`}
+        }
+      `}
+      InputLabelProps={{ disableAnimation: true, shrink: true }}
+      {...props}
+    />
+  );
+};
+
+export { FormInput };
